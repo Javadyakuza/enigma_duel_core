f3a92914a1cb9bccec4737750dcae947aa175e48
Javadyakuza <Javadyakuzaa@gmail.com>
2024-07-28 01:45:31 +0330

    access implemented

--- src/EnigmaDuel.sol
+++ src/EnigmaDuel.sol
@@ -2,16 +2,27 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin-contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin-contracts/access/AccessControl.sol";
 
-contract EnigmaDuel is ERC20 {
+contract EnigmaDuel is ERC20, Ownable, AccessControl {
     // fee and token of the platform
 
     address public EDT;
     uint256 public FEE;
 
-    constructor(address _edt, uint256 _fee) ERC20("Enigma Dule Token", "EDT") {
+
+    mapping (address => uint256 ) public balances;
+
+    constructor(
+        address _edt,
+        uint256 _fee
+    ) ERC20("Enigma Dule Token", "EDT") Ownable(msg.sender) {
         EDT = _edt;
-        Fee = FEE;
+        FEE = _fee;
+        _grantRole(bytes("OWNER"), msg.sender);
+        _setRoleAdmin(bytes("ADMIN"), bytes("OWNER"));
     }
-    
+
+
 }
--- src/Structures.sol
+++ src/Structures.sol
@@ -15,12 +15,4 @@ library Models {
         uint256 prizePool;
         GameRoomStatus status;
     }
-
-    /// Data structure for saving the admin data
-    struct Admin {
-        address addr;
-        uint256 balance;
-        uint256 collected;
-    }
-
 }
