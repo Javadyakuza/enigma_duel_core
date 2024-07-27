29c670e84a7059ada168f94072e14366bdcb10ae
Javadyakuza <Javadyakuzaa@gmail.com>
2024-07-28 00:58:47 +0330

    contructor add

--- src/EnigmaDuel.sol
+++ src/EnigmaDuel.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.19;
 
-contract EnigmaDule 
+import {ERC20} from "@openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+contract EnigmaDuel is ERC20 {
+    // fee and token of the platform
+
+    address public EDT;
+    uint256 public FEE;
+
+    constructor(address _edt, uint256 _fee) ERC20("Enigma Dule Token", "EDT") {
+        EDT = _edt;
+        Fee = FEE;
+    }
+    
+}
--- src/Errors.sol
+++ src/Errors.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.19;
 
 library CodeDuelErrors {
-    
+
     uint8 constant GameRoomNotStarted = 11;
     uint8 constant GameRoomAlreadyStarted = 12;
     uint8 constant GameRoomLoadError = 13;
