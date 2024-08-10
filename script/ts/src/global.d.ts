c7ff799cc8081c153c6bade1d3c576140452829d
Javadyakuza <Javadyakuzaa@gmail.com>
2024-08-11 01:56:51 +0330

    lib fix

---
+++ script/ts/src/global.d.ts
@@ -0,0 +1,6 @@
+// global.d.ts
+
+interface Window {
+    ethereum: any;  // You can replace 'any' with a more specific type if you know it
+  }
+  
--- script/ts/src/helpers.ts
+++ script/ts/src/helpers.ts
@@ -1,5 +1,7 @@
-import { ethers, BigNumber, BytesLike } from "ethers";
+import { ethers, BigNumberish, BytesLike, toBigInt } from "ethers";
 import { Balance, GameRoom } from "./types";
+import {IEnigmaDuelClient} from "./interface";
+
 // Assume the ABI and contract address are provided
 const ABI = [ /* Contract ABI goes here */ ];
 const CONTRACT_ADDRESS = "0xYourContractAddressHere";
@@ -7,13 +9,13 @@ const CONTRACT_ADDRESS = "0xYourContractAddressHere";
 const enigmaDuelClient = (provider: ethers.Provider, signer: ethers.Signer): IEnigmaDuelClient => {
   const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
 
-  const depositEDT = async (depositAmount: BigNumber): Promise<BigNumber> => {
+  const depositEDT = async (depositAmount: BigNumberish): Promise<BigNumberish> => {
     const tx = await contract.depositEDT(depositAmount);
     const receipt = await tx.wait();
     return receipt.events[0].args._new_balance;
   };
 
-  const withdrawEDT = async (withdrawAmount: BigNumber): Promise<BigNumber> => {
+  const withdrawEDT = async (withdrawAmount: BigNumberish): Promise<BigNumberish> => {
     const tx = await contract.withdrawEDT(withdrawAmount);
     const receipt = await tx.wait();
     return receipt.events[0].args._new_balance;
@@ -27,11 +29,11 @@ const enigmaDuelClient = (provider: ethers.Provider, signer: ethers.Signer): IEn
     return contract.getGameRoom(gameRoomKey);
   };
 
-  const getFEE = async (): Promise<BigNumber> => {
+  const getFEE = async (): Promise<BigNumberish> => {
     return contract.getFEE();
   };
 
-  const getDRAW_FEE = async (): Promise<BigNumber> => {
+  const getDRAW_FEE = async (): Promise<BigNumberish> => {
     return contract.getDRAW_FEE();
   };
 
@@ -50,13 +52,3 @@ const enigmaDuelClient = (provider: ethers.Provider, signer: ethers.Signer): IEn
   };
 };
 
-// Usage example
-(async () => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const client = enigmaDuelClient(provider, signer);
-
-  // Deposit 10 EDT tokens (example value)
-  const newBalance = await client.depositEDT(BigNumber.from("10000000000000000000"));
-  console.log(`New Balance after deposit: ${newBalance.toString()}`);
-})();
